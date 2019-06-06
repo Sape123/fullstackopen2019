5944d3e37ffdb4f614cdd6b92b86ac4f6e213d5f
kazel <samulipelt@gmail.com>
2019-06-06 19:17:52 +0300

    123

---
+++ osa1/kurssitiedot/src/index.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+
+
+
+
+const App = () => {
+  const course = {
+    
+    name: 'Half Stack application development',
+
+     parts: [
+
+
+
+
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+   ]
+  }
+  return (
+    <div>
+    <Header otsikko={course.name}/>
+    <Content parts={course.parts}/>
+    
+    <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+
+const Content = (props) => {
+
+
+  return(
+  <div>
+
+
+   
+    <Part nimi1={props.parts[0].name} tehtävät1={props.parts[0].exercises}/>
+    <Part nimi2={props.parts[1].name} tehtävät2={props.parts[1].exercises}/>
+     <Part nimi3={props.parts[2].name} tehtävät3={props.parts[2].exercises}/>
+
+  </div>
+      )
+  }
+
+
+const Header = (props) => {
+
+return(
+<div>
+<h1>{props.otsikko}</h1>
+</div>
+    )
+}
+
+
+    const Total = (props) => {
+
+    return(
+        <div>
+<p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises +props.parts[2].exercises}</p>
+
+          </div>
+    )
+    }
+
+
+
+
+
+    const Part = (props) =>{
+     
+return(
+
+<p> {props.nimi1} {props.tehtävät1}
+{props.nimi2} {props.tehtävät2}
+{props.nimi3} {props.tehtävät3}
+
+</p>
+
+)
+    }
+
+
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ osa1/tiedosto.js
@@ -0,0 +1,6 @@
+const sum = (p1, p2) => {
+    console.log(p1)
+    console.log(p2)
+    return p1 + p2
+  }
+  const result = sum(1, 5)
